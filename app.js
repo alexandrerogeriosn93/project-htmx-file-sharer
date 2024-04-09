6a470c1bf11fcb851153c454b9c9259ed57f9b06
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-09 18:35:49 -0300

    feat: create Home page and config tailwindcss

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.render("layout", { title: "Home", template: "index" });
+});
+
 app.listen(port, () => console.log(`Server running on port: ${port}`));
